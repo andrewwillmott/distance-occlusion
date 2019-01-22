ed8f02c0d82592762fd58a919cd101bbf69908af
Andrew Willmott <awillmott+git@gmail.com>
2019-01-22 14:33:45 +0000

    forgot the vanilla TrailingZeroes impl -- fixed

--- DistanceOcclusionLib.cpp
+++ DistanceOcclusionLib.cpp
@@ -43,8 +43,12 @@ namespace
 #elif defined(__GNUC__)
     #define TrailingZeroes32 __builtin_ctz
 #else
+    // Hash table approach is actually comparable in speed to intrinsics, faster than hierarchical bit tricks
+    constexpr int kTrailingZeroesHashTable[37] = { 32, 0, 1, 26, 2, 23, 27, 0, 3, 16, 24, 30, 28, 11, 0, 13, 4, 7, 17, 0, 25, 22, 31, 15, 29, 10, 12, 6, 0, 21, 14, 9, 5, 20, 8, 19, 18 };
+
     int TrailingZeroes32(uint32_t x)
     {
+        return kTrailingZeroesHashTable[(x & -x) % 37];
     }
 #endif
 
